1bf0034feb2705778a806c47970eb7c09b3169e4
Shriya tp <shriyatp99@gmail.com>
2018-10-29 15:39:10 +0530

    Update preload_train_minibatch.py

--- utils/preload_train_minibatch.py
+++ utils/preload_train_minibatch.py
@@ -13,16 +13,14 @@ batch_size = 64
 max_length = 70 # for 3 sentences
 
 
-with open('../../data/glove/tokenToIndex', 'r') as f:
+with open('/content/auth_id/tokenToIndex', 'r') as f:
     try:
         wordToIndex = json.load(f)
     # if the file is empty the ValueError will be thrown
     except ValueError:
         wordToIndex = {}
 
-
-cwd = os.getcwd()
-data_path = cwd + '/../dataset/C50/C50train'
+data_path = '/content/C50/C50train'
 #auth_sent_num = fdt.file2auth_sent_num(data_path)  # read in the training data
 auth_sentbundle_num = fdt.file2auth_sentbundle_num(data_path, 3)
 ind = np.arange(len(auth_sentbundle_num))
@@ -32,7 +30,7 @@ raw_data = [auth_sentbundle_num[i] for i in index ]
 #batch_list = rmb.read_minibatch(raw_data, batch_size, max_length, False)
 batch_list = rmba.read_minibatch(raw_data, batch_size, max_length, False)
 
-output = open('../../data/batch_data/C50/data_train_bundle_seq.pkl', 'wb')
+output = open('/content/auth_id/data_train_bundle_seq.pkl', 'wb')
 pickle.dump(batch_list, output, -1)
 output.close()
 
